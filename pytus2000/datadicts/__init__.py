86093429e9031b64a50ebc60aa5cce9302e7dd52
Tim Tröndle <tt397@cam.ac.uk>
2016-12-22 14:30:45 +0100

    Add variable label to VariableEnum in datadict
    Fixes #3

--- pytus2000/datadicts/__init__.py
+++ pytus2000/datadicts/__init__.py
@@ -1,2 +1,10 @@
 """This subpackage contains all data dictionaries."""
 # The Python source code gets auto-generated and this package is intentially empty.
+from enum import Enum
+
+
+class VariableEnum(Enum):
+
+    def __init__(self, position, label):
+        self.position = position
+        self.label = label
